bff8a171736f0bef61ccec501818d647a09f6458
Zheng Qu <quzhengrobot@gmail.com>
2019-03-19 09:56:03 +0100

    added path functionalities

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -298,4 +301,114 @@ func getExecPath() string {
 		panic(err)
 	}
 	return filepath.Dir(ex)
+}
+
+func getAbsPaths(root string, children []string) []string {
+	res := make([]string, len(children))
+	for i, child := range children {
+		res[i] = filepath.Join(root, child)
+	}
+	return res
+}
+
+func copyAll(src, dst []string) error {
+	if len(src) != len(dst) {
+		return errors.New("src and dst must have same length")
+	}
+
+	for _, s := range src {
+		if !pathExists(s) {
+			return fmt.Errorf("%s does not exist", s)
+		}
+	}
+	return nil
+}
+
+// File copies a single file from src to dst
+func copyFile(src, dst string) error {
+	var err error
+	var srcfd *os.File
+	var dstfd *os.File
+	var srcinfo os.FileInfo
+
+	if srcfd, err = os.Open(src); err != nil {
+		return err
+	}
+	defer srcfd.Close()
+
+	if dstfd, err = os.Create(dst); err != nil {
+		return err
+	}
+	defer dstfd.Close()
+
+	if _, err = io.Copy(dstfd, srcfd); err != nil {
+		return err
+	}
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+	return os.Chmod(dst, srcinfo.Mode())
+}
+
+// Dir copies a whole directory recursively
+func copyDir(src string, dst string) error {
+	var err error
+	var fds []os.FileInfo
+	var srcinfo os.FileInfo
+
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = ioutil.ReadDir(src); err != nil {
+		return err
+	}
+	for _, fd := range fds {
+		srcfp := path.Join(src, fd.Name())
+		dstfp := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = copyDir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err = copyFile(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
+}
+
+func pathExists(path string) bool {
+	if _, err := os.Stat(path); !os.IsNotExist(err) {
+		return true
+	}
+	return false
+}
+
+type pathType int
+
+const (
+	filePath pathType = iota
+	dirPath
+	noPath
+)
+
+func getPathType(path string) pathType {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return noPath
+	}
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		return dirPath
+	case mode.IsRegular():
+		return filePath
+	}
+	return noPath
 }
